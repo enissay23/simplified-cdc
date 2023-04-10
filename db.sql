47241fcd068a9449f2268bda5109b01d29deb9df
yassinephp23 <yassine.charjami@gmail.com>
2023-04-10 22:29:59 +0200

    initialize db.sql

---
+++ db.sql
@@ -0,0 +1,18 @@
+--
+-- Database :  `cdcSimplified`
+--
+
+-- --------------------------------------------------------
+
+--
+-- table structure : `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
